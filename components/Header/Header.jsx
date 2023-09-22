efe864f7e909b4fa5478d753c957e16938eaa6ad
Vyalov Vadim <vyalov.vadim@gmail.com>
2023-09-23 00:26:46 +0300

    Update Header.jsx

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -8,15 +8,15 @@ import useScreen from '../../hooks/useScreen';
 //import config from '../../public/config/index.json';
 
 const Header = ({ className = '' }) => {
-	const { isM } = useScreen();
-	//const {menu} = config;
-	return (
-		<header
-			className={`text-white absolute z-10 top left w-[100vw] px-[20px] pt-[36px] t:px-[32px] t:pt-[25px] d:px-[24px] d:pt-[48px] ${className}`}
-		>
-			{isM ? <Navbar /> : <p>'BurgerMenu'</p>}
-		</header>
-	);
+  const { isM } = useScreen();
+  //const {menu} = config;
+  return (
+    <header
+      className={`text-white absolute z-10 top left w-[100vw] px-[20px] pt-[36px] t:px-[32px] t:pt-[25px] d:px-[24px] d:pt-[48px] ${className}`}
+    >
+      {isM ? <Navbar /> : <p>"BurgerMenu"</p>}
+    </header>
+  );
 };
 
 export default Header;
